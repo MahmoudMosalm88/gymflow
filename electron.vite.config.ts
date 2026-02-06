5afec3c27fbf9506a866e18eb60c647677e53c31
Mahmoud <mahmoud88mesalm@gmail.com>
2026-02-06 16:28:25 +0200

    Fix dev server host for renderer

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -20,6 +20,11 @@ export default defineConfig({
         '@': resolve('src/renderer/src')
       }
     },
+    server: {
+      host: '127.0.0.1',
+      port: 5173,
+      strictPort: true
+    },
     plugins: [react()]
   }
 })
