c70df1bd96d9d3e1c6547e851b3efc20299227d6
Mahmoud <mahmoud88mesalm@gmail.com>
2026-03-08 02:01:27 +0200

    chore(csp): allow cloudflare insights script and beacon

--- saas-web/next.config.mjs
+++ saas-web/next.config.mjs
@@ -5,11 +5,11 @@ const cspDirectives = [
   "base-uri 'self'",
   "object-src 'none'",
   "frame-ancestors 'none'",
-  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://www.google.com/recaptcha/ https://www.recaptcha.net/recaptcha/",
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://www.google.com/recaptcha/ https://www.recaptcha.net/recaptcha/ https://static.cloudflareinsights.com",
   "style-src 'self' 'unsafe-inline'",
   "img-src 'self' data: blob: https:",
   "font-src 'self' https://fonts.gstatic.com",
-  "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.firebase.com https://securetoken.googleapis.com https://identitytoolkit.googleapis.com https://www.googleapis.com https://apis.google.com https://accounts.google.com https://recaptchaenterprise.googleapis.com https://www.google.com/recaptcha/ https://www.recaptcha.net/recaptcha/ https://www.google.com",
+  "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.firebase.com https://securetoken.googleapis.com https://identitytoolkit.googleapis.com https://www.googleapis.com https://apis.google.com https://accounts.google.com https://recaptchaenterprise.googleapis.com https://www.google.com/recaptcha/ https://www.recaptcha.net/recaptcha/ https://www.google.com https://cloudflareinsights.com https://static.cloudflareinsights.com",
   "frame-src 'self' https://*.firebaseapp.com https://*.web.app https://accounts.google.com https://*.google.com https://www.recaptcha.net https://recaptcha.google.com",
   "worker-src 'self' blob:"
 ].join("; ");
